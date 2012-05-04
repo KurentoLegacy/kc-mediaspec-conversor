ca61eb61f84c41a012f791e2e735141f4461780e
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-05-04 12:37:31 +0200

    Add license information to source files

--- src/main/java/com/kurento/commons/media/format/conversor/SdpConversor.java
+++ src/main/java/com/kurento/commons/media/format/conversor/SdpConversor.java
@@ -1,3 +1,20 @@
+/*
+ * kc-mediaspec-conversor: Media description conversor between various formats
+ * Copyright (C) 2012 Tikal Technologies
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.kurento.commons.media.format.conversor;
 
 import gov.nist.javax.sdp.fields.AttributeField;
