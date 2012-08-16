bbad94ba8355b38d45be14cd21d925cdbd73e743
Miguel París Díaz <mparisdiaz@gmail.com>
2012-08-16 09:39:35 +0200

    Add functionality to parse and serialize extra-attr in SDP.

--- src/main/java/com/kurento/commons/media/format/conversor/FormatParametersConversor.java
+++ src/main/java/com/kurento/commons/media/format/conversor/FormatParametersConversor.java
@@ -21,18 +21,19 @@ import gov.nist.javax.sdp.fields.SDPField;
 
 import javax.sdp.SdpConstants;
 
+import com.kurento.mediaspec.Fraction;
 import com.kurento.mediaspec.Payload;
 import com.kurento.mediaspec.PayloadRtp;
 
 class FormatParametersConversor {
 
-	protected static void parseFormatParameters(Payload payload, String value) {
-		PayloadRtp rtp;
+	public static final String EXTRA_ATTRIBUTES = "extra-attr";
 
+	protected static void parseFormatParameters(Payload payload, String value) {
 		if (!payload.isSetRtp())
 			return;
 
-		rtp = payload.getRtp();
+		PayloadRtp rtp = payload.getRtp();
 
 		if (rtp.getCodecName().equalsIgnoreCase("AMR")) {
 			int idx = value.indexOf(" ");
@@ -65,6 +66,60 @@ class FormatParametersConversor {
 			return SDPField.ATTRIBUTE_FIELD + SdpConstants.FMTP + ":"
 					+ rtp.getId() + " " + sb.toString() + "\r\n";
 		else
+			return "";
+	}
+
+	protected static void parseExtraAttributes(Payload payload, String value) {
+		if (!payload.isSetRtp())
+			return;
+
+		PayloadRtp rtp = payload.getRtp();
+
+		int idx = value.indexOf(" ");
+		String paramsStr = value.substring(idx + 1);
+		String[] params = paramsStr.split(";");
+		for (String param : params) {
+			String[] pk = param.split("=");
+			if (pk.length != 2) {
+				continue;
+			}
+			if (pk[0].equalsIgnoreCase("w"))
+				rtp.setWidth(Integer.parseInt(pk[1]));
+			else if (pk[0].equalsIgnoreCase("h"))
+				rtp.setHeight(Integer.parseInt(pk[1]));
+			else if (pk[0].equalsIgnoreCase("fr")) {
+				String[] frac = pk[1].split("/");
+				rtp.setFramerate(new Fraction(Integer.parseInt(frac[0]),
+						Integer.parseInt(frac[1])));
+			} else
+				rtp.putToExtraParams(pk[0], pk[1]);
+		}
+	}
+
+	protected static String getExtraAttributes(PayloadRtp rtp) {
+		StringBuilder sb = new StringBuilder();
+		if (rtp.isSetWidth())
+			sb.append("w=" + rtp.getWidth() + ";");
+		if (rtp.isSetHeight())
+			sb.append("h=" + rtp.getHeight() + ";");
+		if (rtp.isSetFramerate())
+			sb.append("fr=" + rtp.getFramerate().getNum() + "/"
+					+ rtp.getFramerate().getDenom() + ";");
+		if (rtp.isSetExtraParams()) {
+			for (String k : rtp.getExtraParams().keySet()) {
+				if (!k.equalsIgnoreCase("octet-align")) {
+					String value = rtp.getExtraParams().get(k);
+					if (value != null)
+						sb.append(k + "=" + value + ";");
+				}
+			}
+		}
+
+		if (sb.length() != 0) {
+			sb.deleteCharAt(sb.length() - 1);
+			return SDPField.ATTRIBUTE_FIELD + EXTRA_ATTRIBUTES + ":"
+					+ rtp.getId() + " " + sb.toString() + "\r\n";
+		} else
 			return "";
 	}
 }
